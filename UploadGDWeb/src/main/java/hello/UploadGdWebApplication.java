e31cb29bcf57233db38812a619299ff4cba268d8
thanhgiau2604 <thanhgiau2604@gmail.com>
2019-03-09 19:53:48 +0700

    Source code update 9/3/2019

---
+++ UploadGDWeb/src/main/java/hello/UploadGdWebApplication.java
@@ -0,0 +1,27 @@
+package hello;
+
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadGdWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadGdWebApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+		};
+	}
+}
+
